261dc873714be5c50900eef13889ca5a65de0fd4
BRTPLMN <bart.paeleman@gmail.com>
2026-05-28 13:49:45 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 // Dit bestand wordt nooit naar de browser gestuurd.
 
 // CLOUD BASED
-//define('HA_URL', 'https://dwiq5ycb969vdm8gjsezohusxk3k27nm.ui.nabu.casa'); 
+//define('HA_URL', ''); 
 
 // LOCAL URL
-define('HA_URL', 'http://192.168.0.220:8123'); 
+define('HA_URL', ''); 
 
 
-define('HA_TOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwYWY5MDBjMDJjNDQ0ZTc5YmU2OTQ4MGVjZTMwMjYzNSIsImlhdCI6MTc3OTUzMDQ5NSwiZXhwIjoyMDk0ODkwNDk1fQ.Wy1yLSSlixpJ9pxfQVB69MicpY7aOxtLMYHsfkwmoNs'); 
+define('HA_TOKEN', ''); 
 ?>
